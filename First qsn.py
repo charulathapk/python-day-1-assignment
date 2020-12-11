60bf1bf8546084c0cf25f97ca84fcce91f7f316f
charulathapk <75816664+charulathapk@users.noreply.github.com>
2020-12-11 11:33:16 +0530

    Update First qsn.py

--- First qsn.py
+++ First qsn.py
@@ -1,2 +1,11 @@
 # python-day-1-assignment
-python day 1 assignment
+
+ 
+#5 functions of strings in python
+ 
+name = "charu"
+print(name.upper())
+print(name.lower())
+print(name.islower())
+print(name.isupper())
+print(len(name))
