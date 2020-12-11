dfb6754543343202bd2ac302d95f993a534af455
charulathapk <75816664+charulathapk@users.noreply.github.com>
2020-12-11 12:21:23 +0530

    Update Third qsn.py

--- Third qsn.py
+++ Third qsn.py
@@ -1,2 +1,11 @@
 # python-day-1-assignment
-python day 1 assignment
+#dictionary
+person = {'name': 'Phill', 'age': 22}
+
+print('Name: ', person.get('name'))
+print('Age: ', person.get('age'))
+print(person.items())
+print(person.copy())
+print(person.keys())
+print(person.values())
+print ('name: ',person.get('name'))
