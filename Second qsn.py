e8261f242074e0978e1171b4ec4c18401a69aaac
charulathapk <75816664+charulathapk@users.noreply.github.com>
2020-12-11 11:57:46 +0530

    Update Second qsn.py

--- Second qsn.py
+++ Second qsn.py
@@ -1,2 +1,21 @@
 # python-day-1-assignment
-python day 1 assignment
+
+odd = [1, 3, 5]
+
+odd.append(7)
+
+print(odd)
+
+odd.extend([9, 11, 13])
+
+print(odd)
+
+odd.insert(1,3)
+
+print(odd)
+
+print(odd.count(3))
+
+print(max(odd))
+
+print(min(odd))
